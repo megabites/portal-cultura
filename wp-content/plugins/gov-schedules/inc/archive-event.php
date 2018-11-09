3d96da89e8a8bef0127aadce3789033cf0687c20
Ricardo Carvalho <rickmanu@gmail.com>
2018-11-09 11:32:27 -0200

    [ADD] Filtro de agendas e marcacao de datas com eventos na agenda completa

--- wp-content/plugins/gov-schedules/inc/archive-event.php
+++ wp-content/plugins/gov-schedules/inc/archive-event.php
@@ -22,8 +22,46 @@ get_header();
 						<h1 class="page-title text-center mt-1">Agenda</h1>
 					</header>
 
+					<?php
+
+					$args = array(
+						'post_type' => 'event',
+						'posts_per_page' => -1
+					);
+
+					$all_events_query = new WP_Query($args);
+					$event_dates = array();
+
+					if( $all_events_query->have_posts() ):
+						while ($all_events_query->have_posts()) : $all_events_query->the_post();
+
+							$start_date = get_post_meta( get_the_ID(), 'dados_do_evento_data-de-incio', true );
+							$end_date = get_post_meta( get_the_ID(), 'dados_do_evento_data-de-fim', true );
+							$raw_start_date = explode(' ', $start_date );
+							$raw_end_date = explode(' ', $end_date );
+							array_push($event_dates, str_replace( '-', '', $raw_start_date[0]));
+							array_push($event_dates, str_replace( '-', '', $raw_end_date[0]));
+
+						endwhile;
+					endif; ?>
+
 					<div class="agenda-archive">
-						<div id="archive-datepicker"></div>
+
+						<div class="event-cats-selector mb-5">
+							<?php
+							$event_cats = get_terms( 'event-category', array(
+								'hide_empty' => 0
+							) ); ?>
+							<div class="input-group" style="max-width: 50%;margin: auto;">
+								<select class="form-control" style="height: 50px; border-radius: 30px;">
+									<?php foreach ($event_cats as $cat): ?>
+										<option value="<?php echo $cat->slug; ?>"><?php echo $cat->name; ?></option>
+									<?php endforeach; ?>
+								</select>
+							</div>
+						</div>
+
+						<div id="archive-datepicker" data-event-days='<?php echo json_encode($event_dates); ?>'></div>
 
 						<div id="agenda" class="gs-agenda-container mt-5">
 							<div class="daypicker-wrapper">
@@ -37,7 +75,7 @@ get_header();
 										date_sub($date, date_interval_create_from_date_string($i . ' days')); ?>
 
 										<li>
-											<a href="#" data-day="<?php echo date_format($date, 'Y-m-d'); ?>">
+											<a href="#" data-day="<?php echo date_format($date, 'Y-m-d'); ?>" title="<?php echo strftime('%d/%m/%Y', strtotime( date_format($date, 'Y-m-d') ) ); ?>">
 												<span><?php echo date_format($date, 'd'); ?></span>
 												<small><?php echo strftime('%a', strtotime(date_format($date, 'Y-m-d'))); ?></small>
 											</a>
@@ -51,7 +89,7 @@ get_header();
 										date_add($date, date_interval_create_from_date_string($i . ' days')); ?>
 
 										<li <?php echo $i === 0 ? 'class="selected"' : ''; ?>>
-											<a href="#" data-day="<?php echo date_format($date, 'Y-m-d'); ?>">
+											<a href="#" data-day="<?php echo date_format($date, 'Y-m-d'); ?>" title="<?php echo strftime('%d/%m/%Y', strtotime( date_format($date, 'Y-m-d') ) ); ?>">
 												<span><?php echo date_format($date, 'd'); ?></span>
 												<small><?php echo strftime('%a', strtotime(date_format($date, 'Y-m-d'))); ?></small>
 											</a>
@@ -104,10 +142,11 @@ get_header();
 										)
 									);
 
-									$query = new WP_Query($args);
+									$this_day_events_query = new WP_Query($args);
 
-									if( $query->have_posts() ):
-										while ($query->have_posts()) : $query->the_post();
+									if( $this_day_events_query->have_posts() ):
+
+										while ($this_day_events_query->have_posts()) : $this_day_events_query->the_post();
 
 											$locaction = get_post_meta( get_the_ID(), 'dados_do_evento_location', true );
 											$date = get_post_meta( get_the_ID(), 'dados_do_evento_data-de-incio', true );
@@ -119,7 +158,7 @@ get_header();
 												</div>
 
 												<div class="info">
-													<?php the_title('<h2>', '</h2>'); ?>
+													<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 
 													<div class="additional">
 														<span class="location icon icon-location"><?php echo $locaction; ?></span>
@@ -132,14 +171,14 @@ get_header();
 										<?php
 
 										endwhile; wp_reset_query();
+
 									else: ?>
 
 										<div class="event-item empty">
 											<span class="location">Sem compromissos oficiais nesta data.</span>
 										</div>
 
-									<?php endif;
-									?>
+									<?php endif; ?>
 								</div>
 							</div>
 						</div>
