3c8e5a1c077da541399f29c46d96478924c15300
Daniel Moura <daniel.mouraa@gmail.com>
2018-12-20 16:50:02 -0200

    [UPDATE] Exibindo titulo nos banners de multimidia

--- wp-content/themes/idg-wp/inc/idg-widgets.php
+++ wp-content/themes/idg-wp/inc/idg-widgets.php
@@ -252,7 +252,7 @@ class IDG_Banners extends WP_Widget {
 							<?php if ( ! empty($instance['link_title'][ $i ]) ): ?>
 								<div class="box-body">
 									<h3 class="box-title">
-										<?php echo $instance['link_title'][ $i ] ? 'title="' . $instance['link_title'][ $i ] . '"' : ''; ?>
+										<?php echo $instance['link_title'][ $i ]; ?>
 									</h3>
 								</div>
 							<?php endif; ?>
