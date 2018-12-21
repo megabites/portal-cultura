39e2bedd908698bcc9247c29c7ba97e5f52ef398
Daniel Moura <daniel.mouraa@gmail.com>
2018-12-21 14:42:51 -0200

    [UPDATE] Removendo carrosel secretarias e comentarios

--- wp-content/themes/idg-wp/page-secretarias.php
+++ wp-content/themes/idg-wp/page-secretarias.php
@@ -22,7 +22,7 @@ get_header();
     <div class="container">
       <?php the_breadcrumb (); ?>
 
-<?/*
+<?php /*
       <div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">
         <div class="carousel-inner">
           <div class="carousel-item active">
@@ -66,7 +66,7 @@ get_header();
       </div>
     </div>
 
-<?/*
+<?php /*
     <div id="conteudo-especifico" class="pt-5" style="background-color: #19224d; padding-bottom: 80px; background-image: url("data:image/svg+xml,%3Csvg width='40' height='12' viewBox='0 0 40 12' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M0 6.172L6.172 0h5.656L0 11.828V6.172zm40 5.656L28.172 0h5.656L40 6.172v5.656zM6.172 12l12-12h3.656l12 12h-5.656L20 3.828 11.828 12H6.172zm12 0L20 10.172 21.828 12h-3.656z' fill='%23ffffff' fill-opacity='0.05' fill-rule='evenodd'/%3E%3C/svg%3E");">
       <div class="container">
         <h2 class="text-center mb-4" style="color: #FFF;">Conteúdo Específico</h2>
