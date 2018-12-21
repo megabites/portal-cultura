d94ee72350b0ad0ec47844e11ae38a8e6db7ea09
Ricardo Carvalho <rickmanu@gmail.com>
2018-12-21 13:43:00 -0200

    [ADD] Desabilitar mobile breakpoints

--- wp-content/themes/idg-wp/header.php
+++ wp-content/themes/idg-wp/header.php
@@ -15,7 +15,7 @@
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="viewport" content="width=device-width, initial-scale=0, shrink-to-fit=no">
 
 		<link rel="profile" href="https://gmpg.org/xfn/11">
 
