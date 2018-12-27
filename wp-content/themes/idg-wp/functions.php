a00b6155fbc08340b84f83df58bbdcb80d2c55a2
Ricardo Carvalho <rickmanu@gmail.com>
2018-12-27 11:25:56 -0200

    [ADD] Escondendo informacao de atualizacao para usuarios nao administradores

--- wp-content/themes/idg-wp/functions.php
+++ wp-content/themes/idg-wp/functions.php
@@ -351,3 +351,15 @@ function embeded_youtube_video_id($url) {
 	}
 	return $post_content;
 }
+
+/**
+ * Hiding the update notification for non admin users
+ *
+ */
+function hide_update_notice_to_all_but_admin_users()
+{
+	if (!current_user_can('update_core')) {
+		remove_action( 'admin_notices', 'update_nag', 3 );
+	}
+}
+add_action( 'admin_head', 'hide_update_notice_to_all_but_admin_users', 1 );
