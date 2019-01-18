d724b7e79758b9b1d99e9c6e99ca3664551657a4
Daniel Moura <daniel.mouraa@gmail.com>
2019-01-18 10:05:18 -0200

    [UPDATE] Criando bloco de rolagem horizontal para mobile na home

--- wp-content/themes/idg-wp/home.php
+++ wp-content/themes/idg-wp/home.php
@@ -27,122 +27,122 @@ get_header();
 					<div class="col-lg-12">
 						<h2 class="section-title mb-5 text-center">Notícias</h2>
 					</div>
-					<?php
-					/*$args = [];
-					if( get_option('idg-wp_theme_options_news_sections') ){
-						$args['category_name'] = get_option('idg-wp_theme_options_news_sections');
-					}
-					if( get_option('idg-wp_theme_options_news_sections_items') ){
-						$args['posts_per_page'] = get_option('idg-wp_theme_options_news_sections_items');
-					} else {
-						$args['posts_per_page'] = 3;
-					}*/
-					$args      = array(
-						'posts_per_page' => 1,
-						'category_name'  => 'destaquinho-1'
-					);
-					$news_query = new WP_Query( $args ); ?>
 
-					<?php if ( $news_query->have_posts() ) : ?>
+					<div class="overflow-wrapper">
+						<?php
+						/*$args = [];
+						if( get_option('idg-wp_theme_options_news_sections') ){
+							$args['category_name'] = get_option('idg-wp_theme_options_news_sections');
+						}
+						if( get_option('idg-wp_theme_options_news_sections_items') ){
+							$args['posts_per_page'] = get_option('idg-wp_theme_options_news_sections_items');
+						} else {
+							$args['posts_per_page'] = 3;
+						}*/
+						$args      = array(
+							'posts_per_page' => 1,
+							'category_name'  => 'destaquinho-1'
+						);
+						$news_query = new WP_Query( $args ); ?>
 
-						<?php while ( $news_query->have_posts() ) : $news_query->the_post(); ?>
-							<h2></h2>
-							<div class="col-lg-4 mb-5">
-								<?php
-								if ( has_post_thumbnail() ) {
-									$post_thumb = get_the_post_thumbnail_url();
-								} else {
-									$post_thumb = get_template_directory_uri() . '/assets/img/fake-img.jpg';
-								}
-								?>
-								<div class="highlight-box" style="background-image: url('<?php echo $post_thumb; ?>')">
-									<div class="box-body">
-										<?php if( $post_subtitle = get_post_meta( $post->ID, '_post_subtitle', true ) ): ?>
-											<span class="cat"><?php echo $post_subtitle?></span>
-										<?php endif; ?>
-										<h3 class="box-title">
-											<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-										</h3>
+						<?php if ( $news_query->have_posts() ) : ?>
+
+							<?php while ( $news_query->have_posts() ) : $news_query->the_post(); ?>
+								<div class="col-lg-4 mb-5">
+									<?php
+									if ( has_post_thumbnail() ) {
+										$post_thumb = get_the_post_thumbnail_url();
+									} else {
+										$post_thumb = get_template_directory_uri() . '/assets/img/fake-img.jpg';
+									}
+									?>
+									<div class="highlight-box" style="background-image: url('<?php echo $post_thumb; ?>')">
+										<div class="box-body">
+											<?php if( $post_subtitle = get_post_meta( $post->ID, '_post_subtitle', true ) ): ?>
+												<span class="cat"><?php echo $post_subtitle?></span>
+											<?php endif; ?>
+											<h3 class="box-title">
+												<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+											</h3>
+										</div>
 									</div>
 								</div>
-							</div>
-						<?php endwhile; ?>
+							<?php endwhile; ?>
 
-						<?php wp_reset_postdata(); ?>
+							<?php wp_reset_postdata(); ?>
 
-					<?php endif; ?>
+						<?php endif; ?>
 
-					<?php
-					$args      = array(
-						'posts_per_page' => 1,
-						'category_name'  => 'destaquinho-2'
-					);
-					$news_query = new WP_Query( $args ); ?>
+						<?php
+						$args      = array(
+							'posts_per_page' => 1,
+							'category_name'  => 'destaquinho-2'
+						);
+						$news_query = new WP_Query( $args ); ?>
 
-					<?php if ( $news_query->have_posts() ) : ?>
+						<?php if ( $news_query->have_posts() ) : ?>
 
-						<?php while ( $news_query->have_posts() ) : $news_query->the_post(); ?>
-							<h2></h2>
-							<div class="col-lg-4 mb-5">
-								<?php
-								if ( has_post_thumbnail() ) {
-									$post_thumb = get_the_post_thumbnail_url();
-								} else {
-									$post_thumb = get_template_directory_uri() . '/assets/img/fake-img.jpg';
-								}
-								?>
-								<div class="highlight-box" style="background-image: url('<?php echo $post_thumb; ?>')">
-									<div class="box-body">
-										<?php if( $post_subtitle = get_post_meta( $post->ID, '_post_subtitle', true ) ): ?>
-											<span class="cat"><?php echo $post_subtitle?></span>
-										<?php endif; ?>
-										<h3 class="box-title">
-											<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-										</h3>
+							<?php while ( $news_query->have_posts() ) : $news_query->the_post(); ?>
+								<div class="col-lg-4 mb-5">
+									<?php
+									if ( has_post_thumbnail() ) {
+										$post_thumb = get_the_post_thumbnail_url();
+									} else {
+										$post_thumb = get_template_directory_uri() . '/assets/img/fake-img.jpg';
+									}
+									?>
+									<div class="highlight-box" style="background-image: url('<?php echo $post_thumb; ?>')">
+										<div class="box-body">
+											<?php if( $post_subtitle = get_post_meta( $post->ID, '_post_subtitle', true ) ): ?>
+												<span class="cat"><?php echo $post_subtitle?></span>
+											<?php endif; ?>
+											<h3 class="box-title">
+												<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+											</h3>
+										</div>
 									</div>
 								</div>
-							</div>
-						<?php endwhile; ?>
+							<?php endwhile; ?>
 
-						<?php wp_reset_postdata(); ?>
+							<?php wp_reset_postdata(); ?>
 
-					<?php endif; ?>
+						<?php endif; ?>
 
-					<?php
-					$args      = array(
-						'posts_per_page' => 1,
-						'category_name'  => 'destaquinho-3'
-					);
-					$news_query = new WP_Query( $args ); ?>
+						<?php
+						$args      = array(
+							'posts_per_page' => 1,
+							'category_name'  => 'destaquinho-3'
+						);
+						$news_query = new WP_Query( $args ); ?>
 
-					<?php if ( $news_query->have_posts() ) : ?>
+						<?php if ( $news_query->have_posts() ) : ?>
 
-						<?php while ( $news_query->have_posts() ) : $news_query->the_post(); ?>
-							<h2></h2>
-							<div class="col-lg-4 mb-5">
-								<?php
-								if ( has_post_thumbnail() ) {
-									$post_thumb = get_the_post_thumbnail_url();
-								} else {
-									$post_thumb = get_template_directory_uri() . '/assets/img/fake-img.jpg';
-								}
-								?>
-								<div class="highlight-box" style="background-image: url('<?php echo $post_thumb; ?>')">
-									<div class="box-body">
-										<?php if( $post_subtitle = get_post_meta( $post->ID, '_post_subtitle', true ) ): ?>
-											<span class="cat"><?php echo $post_subtitle?></span>
-										<?php endif; ?>
-										<h3 class="box-title">
-											<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-										</h3>
+							<?php while ( $news_query->have_posts() ) : $news_query->the_post(); ?>
+								<div class="col-lg-4 mb-5">
+									<?php
+									if ( has_post_thumbnail() ) {
+										$post_thumb = get_the_post_thumbnail_url();
+									} else {
+										$post_thumb = get_template_directory_uri() . '/assets/img/fake-img.jpg';
+									}
+									?>
+									<div class="highlight-box" style="background-image: url('<?php echo $post_thumb; ?>')">
+										<div class="box-body">
+											<?php if( $post_subtitle = get_post_meta( $post->ID, '_post_subtitle', true ) ): ?>
+												<span class="cat"><?php echo $post_subtitle?></span>
+											<?php endif; ?>
+											<h3 class="box-title">
+												<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+											</h3>
+										</div>
 									</div>
 								</div>
-							</div>
-						<?php endwhile; ?>
+							<?php endwhile; ?>
 
-						<?php wp_reset_postdata(); ?>
+							<?php wp_reset_postdata(); ?>
 
-					<?php endif; ?>
+						<?php endif; ?>
+					</div>
 				</div>
 				<div class="col-lg-12 text-center">
 					<a href="<?php echo home_url( '/categoria/noticias/' ); ?>" class="btn text-uppercase mt-1">Mais
@@ -151,14 +151,16 @@ get_header();
 			</div>
 		</section>
 
-		<section class="services mt-5 mb-5 pt-4">
+		<section id="services" class="mt-5 mb-5 pt-4">
 			<div class="container">
 				<div class="row">
-					<?php
-					if ( is_active_sidebar( 'services-widgets-area' ) ) :
-						dynamic_sidebar( 'services-widgets-area' );
-					endif;
-					?>
+					<div class="overflow-wrapper">
+						<?php
+						if ( is_active_sidebar( 'services-widgets-area' ) ) :
+							dynamic_sidebar( 'services-widgets-area' );
+						endif;
+						?>
+					</div>
 				</div>
 			</div>
 		</section>
@@ -173,49 +175,57 @@ get_header();
 			</div>
 		</section>
 
-		<section class="pt-5 pb-5 mb-5">
+		<section id="secretarias" class="pt-5 pb-5 mb-5">
 			<div class="container">
 				<div class="row">
 					<div class="col-lg-12">
 						<h2 class="section-title mb-5 text-center">Conheça a Secretaria</h2>
 					</div>
-					<?php
-					if ( is_active_sidebar( 'meet-the-ministry-widgets-area' ) ) :
-						dynamic_sidebar( 'meet-the-ministry-widgets-area' );
-					endif;
-					?>
+
+					<div class="overflow-wrapper">
+						<?php
+						if ( is_active_sidebar( 'meet-the-ministry-widgets-area' ) ) :
+							dynamic_sidebar( 'meet-the-ministry-widgets-area' );
+						endif;
+						?>
+					</div>
 				</div>
 			</div>
 		</section>
 
-		<section class="pt-5 pb-5" id="section-content">
+		<section id="section-content" class="pt-5 pb-5">
 			<div class="container">
 				<div class="row">
-					<?php
-					if ( is_active_sidebar( 'content-widgets-area' ) ) :
-						dynamic_sidebar( 'content-widgets-area' );
-					endif;
-					?>
+					<div class="overflow-wrapper">
+						<?php
+						if ( is_active_sidebar( 'content-widgets-area' ) ) :
+							dynamic_sidebar( 'content-widgets-area' );
+						endif;
+						?>
+					</div>
 				</div>
 			</div>
 		</section>
 
-		<section class="mt-5 mb-5">
+		<section id="participacao-social" class="mt-5 mb-5">
 			<div class="container">
 				<div class="row">
 					<div class="col-lg-12">
 						<h2 class="section-title mb-5 text-center">Participação Social</h2>
 					</div>
-					<?php
-					if ( is_active_sidebar( 'social-participation-widgets-area' ) ) :
-						dynamic_sidebar( 'social-participation-widgets-area' );
-					endif;
-					?>
+
+					<div class="overflow-wrapper">
+						<?php
+						if ( is_active_sidebar( 'social-participation-widgets-area' ) ) :
+							dynamic_sidebar( 'social-participation-widgets-area' );
+						endif;
+						?>
+					</div>
 				</div>
 			</div>
 		</section>
 
-		<section class="mt-5" id="multimidia">
+		<section id="multimidia" class="mt-5">
 			<div class="container">
 				<div class="row">
 					<?php get_template_part('template-parts/multimedia-block'); ?>
