3d1230b22432db966edd9ccc6cc63dc3082240cf
Daniel Moura <daniel.mouraa@gmail.com>
2019-01-30 10:12:59 -0200

    [UPDATE] Atualizando link do soundcloud

--- wp-content/themes/idg-wp/footer.php
+++ wp-content/themes/idg-wp/footer.php
@@ -33,7 +33,7 @@
 							<a href="http://instagram.com/culturagovbr" target="_blank">Instagram</a>
 						</li>
 						<li class="soundcloud">
-							<a href="https://soundcloud.com/culturagovbr" target="_blank">Soundcloud</a>
+							<a href="https://soundcloud.com/mincidadania" target="_blank">Soundcloud</a>
 						</li>
 					</ul>
 				</div>
